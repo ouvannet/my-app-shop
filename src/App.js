4683d6399b0be5ace5d9d1d29c45a16019563d78
vannet <netvan163@gmail.com>
2023-02-04 20:41:31 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import React from 'react';
+// import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
+import About from './pages/About';
+import AddEdit from './pages/AddEdit';
+import Home from './pages/Home';
+import View from './pages/View';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+      <div className='APP'>
+        <Header />
+        <ToastContainer position="bottom-center" />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/add' element={<AddEdit/>} />
+          <Route exact path='/update/:id' element={<AddEdit/>} />
+          <Route exact path='/view/:id' element={<View/>} />
+          <Route exact path='/about' element={<About/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
